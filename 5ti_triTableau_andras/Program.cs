c50430640db7f5206940cf27801fcc70f59d2aeb
Andras Corda <115708078+R3tr0-andras@users.noreply.github.com>
2024-01-24 21:54:23 +0100

    - Fin

---
+++ 5ti_triTableau_andras/Program.cs
@@ -0,0 +1,254 @@
+﻿using System.Diagnostics;
+
+namespace _5ti_triTableau_andras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variable utile
+            int[] tableau;
+            string message;
+
+            //Ma librairie
+            traitement programmeTableau = new traitement();
+
+            //Les stopwatch
+            Stopwatch stopWatch = new Stopwatch();  // de base
+            Stopwatch stopWatch1 = new Stopwatch(); // pour les comparaisons de 1 à 6, j'ai pas trouvé de stopwatch.Reset()
+            Stopwatch stopWatch2 = new Stopwatch();
+            Stopwatch stopWatch3 = new Stopwatch();
+            Stopwatch stopWatch4 = new Stopwatch();
+            Stopwatch stopWatch5 = new Stopwatch();
+            do
+            {
+                Console.Clear();
+                // Aficher les options possibles
+                AfficherMenu();
+                string choix = Console.ReadLine();
+
+                switch (choix)
+                {
+                    case "1":
+                        programmeTableau.CreerTableau(out tableau);
+
+                        // Tableau sans tri
+                        Console.WriteLine("\n Tableau non trié \n");
+                        Console.Write(message = programmeTableau.AfficherTableau(tableau, out message));
+
+                        // LANCER LE CHRONO
+                        stopWatch.Start();
+
+                        // Tri par sélection
+                        programmeTableau.TriSelection(tableau);
+
+                        // FIN DU CHRONO
+                        stopWatch.Stop();
+
+                        // Tableau avec tri
+                        Console.WriteLine("\n \n Tableau trié \n");
+                        Console.Write(message = programmeTableau.AfficherTableau(tableau, out message));
+
+                        // Temps en milisecondes
+                        Console.WriteLine("\n \n Temps de traitement : " + stopWatch.ElapsedMilliseconds + " ms");
+                        break;
+
+                    case "2":
+                        programmeTableau.CreerTableau(out tableau);
+                        
+                        // Tableau sans tri
+                        Console.WriteLine("\n Tableau non trié \n");
+                        Console.Write(message = programmeTableau.AfficherTableau(tableau, out message));
+
+                        // LANCER LE CHRONO
+                        stopWatch.Start();
+
+                        // Tri à bulles
+                        programmeTableau.TriBulle(tableau);
+
+                        // FIN DU CHRONO
+                        stopWatch.Stop();
+
+                        // Tableau avec tri
+                        Console.WriteLine("\n \n Tableau trié \n");
+                        Console.Write(message = programmeTableau.AfficherTableau(tableau, out message));
+
+                        // Temps en milisecondes
+                        Console.WriteLine("\n \n Temps de traitement : " + stopWatch.ElapsedMilliseconds + " ms");
+                        break;
+
+                    case "3":
+                        programmeTableau.CreerTableau(out tableau);
+
+                        // Tableau sans tri
+                        Console.WriteLine("\n Tableau non trié \n");
+                        Console.Write(message = programmeTableau.AfficherTableau(tableau, out message));
+
+                        // LANCER LE CHRONO
+                        stopWatch.Start();
+
+                        // Tri par insertion
+                        programmeTableau.TriInsertion(tableau);
+
+                        // FIN DU CHRONO
+                        stopWatch.Stop();
+
+                        // Tableau avec tri
+                        Console.WriteLine("\n \n Tableau trié \n");
+                        Console.Write(message = programmeTableau.AfficherTableau(tableau, out message));
+
+                        // Temps en milisecondes
+                        Console.WriteLine("\n \n Temps de traitement : " + stopWatch.ElapsedMilliseconds + " ms");
+                        break;
+
+                    case "4":
+                        programmeTableau.CreerTableau(out tableau);
+                        
+                        // Tableau sans tri
+                        Console.WriteLine("\n Tableau non trié \n");
+                        Console.Write(message = programmeTableau.AfficherTableau(tableau, out message));
+                        // LANCER LE CHRONO
+                        stopWatch.Start();
+
+                        // Tri intuitif
+                        programmeTableau.TriIntuitif(tableau);
+
+                        // FIN DU CHRONO
+                        stopWatch.Stop();
+
+                        // Tableau avec tri
+                        Console.WriteLine("\n \n Tableau trié \n");
+                        Console.Write(message = programmeTableau.AfficherTableau(tableau, out message));
+
+                        // Temps en milisecondes
+                        Console.WriteLine("\n \n Temps de traitement : " + stopWatch.ElapsedMilliseconds + " ms");
+                        break;
+
+                    case "5":
+                        programmeTableau.CreerTableau(out tableau);
+
+                        // Tableau sans tri
+                        Console.WriteLine("\n Tableau non trié \n");
+                        Console.Write(message = programmeTableau.AfficherTableau(tableau, out message));
+
+                        // LANCER LE CHRONO
+                        stopWatch.Start();
+
+                        // Tri shell
+                        programmeTableau.TriShell(tableau);
+
+                        // FIN DU CHRONO
+                        stopWatch.Stop();
+
+                        // Tableau avec tri
+                        Console.WriteLine("\n \n Tableau trié \n");
+                        Console.Write(message = programmeTableau.AfficherTableau(tableau, out message));
+
+                        // Temps en milisecondes
+                        Console.WriteLine("\n \n Temps de traitement : " + stopWatch.ElapsedMilliseconds + " ms");
+                        break;
+
+                    case "6":
+                        programmeTableau.CreerTableau(out tableau);
+
+                        int[] tableau2 = programmeTableau.CopierTableau(tableau);
+
+                        // LANCER LE CHRONO
+                        stopWatch1.Start();
+
+                        // Tri par sélection
+                        programmeTableau.TriSelection(tableau2);
+
+                        // FIN DU CHRONO
+                        stopWatch1.Stop();
+
+                        // Temps en milisecondes
+                        Console.WriteLine("\n \n Temps de traitement du Tri par sélection : " + stopWatch1.ElapsedMilliseconds + " ms");
+                        programmeTableau.SupprimerTableau(ref tableau2);
+
+                        tableau2 = programmeTableau.CopierTableau(tableau);
+
+                        // LANCER LE CHRONO
+                        stopWatch2.Start();
+
+                        // Tri à bulle
+                        programmeTableau.TriBulle(tableau2);
+
+                        // FIN DU CHRONO
+                        stopWatch2.Stop();
+
+                        // Temps en milisecondes
+                        Console.WriteLine("\n \n Temps de traitement du Tri à bulle : " + stopWatch2.ElapsedMilliseconds + " ms");
+                        programmeTableau.SupprimerTableau(ref tableau2);
+
+                        tableau2 = programmeTableau.CopierTableau(tableau);
+
+                        // LANCER LE CHRONO
+                        stopWatch3.Start();
+
+                        // Tri par insertion
+                        programmeTableau.TriInsertion(tableau2);
+
+                        // FIN DU CHRONO
+                        stopWatch3.Stop();
+
+                        // Temps en milisecondes
+                        Console.WriteLine("\n \n Temps de traitement du Tri par insertion : " + stopWatch3.ElapsedMilliseconds + " ms");
+                        programmeTableau.SupprimerTableau(ref tableau2);
+
+                        tableau2 = programmeTableau.CopierTableau(tableau);
+
+                        // LANCER LE CHRONO
+                        stopWatch4.Start();
+
+                        // Tri intuitif
+                        programmeTableau.TriIntuitif(tableau2);
+
+                        // FIN DU CHRONO
+                        stopWatch4.Stop();
+
+                        // Temps en milisecondes
+                        Console.WriteLine("\n \n Temps de traitement du Tri intuitif : " + stopWatch4.ElapsedMilliseconds + " ms");
+                        programmeTableau.SupprimerTableau(ref tableau2);
+
+                        tableau2 = programmeTableau.CopierTableau(tableau);
+
+                        // LANCER LE CHRONO
+                        stopWatch5.Start();
+
+                        // Tri shell
+                        programmeTableau.TriShell(tableau2);
+
+                        // FIN DU CHRONO
+                        stopWatch5.Stop();
+
+                        // Temps en milisecondes
+                        Console.WriteLine("\n \n Temps de traitement du Tri shell  : " + stopWatch5.ElapsedMilliseconds + " ms");
+                        programmeTableau.SupprimerTableau(ref tableau2);
+                        break;
+
+                    case "Q":
+                        Console.WriteLine("Programme terminé.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Choix non valide. Veuillez réessayer.");
+                        break;
+                }
+                Console.Write("\n Voulez-vous recommencer? (O/N) ");
+            } while (Console.ReadLine().Trim().ToUpper() == "O");
+        }
+
+        static void AfficherMenu()
+        {
+            Console.WriteLine("1. Tri par sélection");
+            Console.WriteLine("2. Tri à bulles");
+            Console.WriteLine("3. Tri par insertion");
+            Console.WriteLine("4. Tri intuitif");
+            Console.WriteLine("5. Tri Shell");
+            Console.WriteLine("6. Comparaison");
+            Console.WriteLine("Q. Quitter");
+            Console.Write("Choix : ");
+        }
+    }
+}
---
+++ 5ti_triTableau_andras/traitement.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5ti_triTableau_andras
+{
+    public struct traitement
+    {
+        public void CreerTableau(out int[] tableau)
+        {
+            string valeurTaille;
+            int taille;
+            do
+            {
+                Console.Write("Entrez la taille du tableau : ");
+                valeurTaille = Console.ReadLine();
+            } while (!int.TryParse(valeurTaille, out taille));
+
+            tableau = GenererTableauAleatoire(taille);
+        }
+
+        public int[] GenererTableauAleatoire(int taille)
+        {
+            int[] tableau = new int[taille];
+            Random random = new Random();
+
+            for (int i = 0; i < taille; i++)
+            {
+                tableau[i] = random.Next(1, 101);
+            }
+            return tableau;
+        }
+
+        public int[] CopierTableau(int[] tableau)
+        {
+            int[] tableau2 = new int[tableau.Length];
+            Array.Copy(tableau, tableau2, tableau.Length);
+            return tableau2;
+        }
+
+        public void SupprimerTableau(ref int[] tableau2)
+        {
+            tableau2 = null; 
+        }
+
+        public string AfficherTableau(int[] tableau, out string message)
+        {
+            message = "";
+            foreach (var nombre in tableau)
+            {
+                message = message + " " + nombre;
+            }
+            return message;
+        }
+
+        public void TriSelection(int[] tableau2)
+        {
+            for (int i = 0; i < tableau2.Length - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < tableau2.Length; j++)
+                {
+                    if (tableau2[j] < tableau2[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                int temp = tableau2[i];
+                tableau2[i] = tableau2[minIndex];
+                tableau2[minIndex] = temp;
+            }
+        }
+        public void TriBulle(int[] tableau2)
+        {
+            int n = tableau2.Length;
+            bool swapped;
+            do
+            {
+                swapped = false;
+                for (int i = 1; i < n; i++)
+                {
+                    if (tableau2[i - 1] > tableau2[i])
+                    {
+                        int temp = tableau2[i - 1];
+                        tableau2[i - 1] = tableau2[i];
+                        tableau2[i] = temp;
+                        swapped = true;
+                    }
+                }
+                n--;
+            } while (swapped);
+        }
+        public void TriInsertion(int[] tableau2)
+        {
+            int n = tableau2.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int key = tableau2[i];
+                int j = i - 1;
+
+                while (j >= 0 && tableau2[j] > key)
+                {
+                    tableau2[j + 1] = tableau2[j];
+                    j = j - 1;
+                }
+                tableau2[j + 1] = key;
+            }
+        }
+        public void TriIntuitif(int[] tableau2)
+        {
+            for (int i = 0; i < tableau2.Length - 1; i++)
+            {
+                for (int j = i + 1; j < tableau2.Length; j++)
+                {
+                    if (tableau2[j] < tableau2[i])
+                    {
+                        int temp = tableau2[i];
+                        tableau2[i] = tableau2[j];
+                        tableau2[j] = temp;
+                    }
+                }
+            }
+        }
+        public void TriShell(int[] tableau2)
+        {
+            int n = tableau2.Length;
+            int gap = n / 2;
+            while (gap > 0)
+            {
+                for (int i = gap; i < n; i += 1)
+                {
+                    int temp = tableau2[i];
+                    int j;
+                    for (j = i; j >= gap && tableau2[j - gap] > temp; j -= gap)
+                    {
+                        tableau2[j] = tableau2[j - gap];
+                    }
+                    tableau2[j] = temp;
+                }
+                gap /= 2;
+            }
+        }
+    }
+}
